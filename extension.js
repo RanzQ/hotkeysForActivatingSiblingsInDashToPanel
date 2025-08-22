7fa322cf9c2f96e03a44cf6b285650adf8db4547
Juha Rantanen <juha@codercoded.com>
2025-08-22 10:13:42 +0300

    feature: jump to group

--- extension.js
+++ extension.js
@@ -5,7 +5,7 @@ import GLib from 'gi://GLib';
 import Shell from 'gi://Shell';
 import Meta from 'gi://Meta';
 
-import * as Main from 'resource:///org/gnome/shell/ui/main.js'
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 import {
   Extension,
@@ -15,26 +15,102 @@ import {
 const GETTEXT_DOMAIN =
   'hotkeysForActivatingSiblingsInDashToPanel@tannerlegvold.gmail.com';
 
-// Taken from https://github.com/home-sweet-gnome/dash-to-panel/blob/v68/src/utils.js#L432
-// Changed the direction to left and right instead of up and down
-function activateSiblingWindow(windows, direction, startWindow) {
-  let windowIndex = windows.indexOf(global.display.focus_window);
-  let nextWindowIndex =
-    windowIndex < 0
-      ? startWindow
-        ? windows.indexOf(startWindow)
-        : 0
-      : windowIndex + (direction == 'left' ? -1 : 1);
+function wrapAround(index, length) {
+  return ((index % length) + length) % length;
+}
 
-  if (nextWindowIndex == windows.length) {
-    nextWindowIndex = 0;
-  } else if (nextWindowIndex < 0) {
-    nextWindowIndex = windows.length - 1;
+function getNextWindowIndex(currentIndex, windowCount, direction = 1) {
+  let nextIndexCandidate = currentIndex + direction;
+  return wrapAround(nextIndexCandidate, windowCount);
+}
+
+function getWindowsOfSameClass(windows, currentWindow) {
+  return windows.filter(window => window.wm_class === currentWindow.wm_class);
+}
+
+function getMostRecentWindowByWMClass(windows, wmClass) {
+  const matchingWindows = windows.filter(window => window.wm_class === wmClass);
+
+  matchingWindows.sort((a, b) => b.get_user_time() - a.get_user_time());
+
+  return matchingWindows.length > 0 ? matchingWindows[0] : null;
+}
+
+// Taken from https://github.com/home-sweet-gnome/dash-to-panel/blob/v68/src/utils.js#L432
+// Changed the direction to left and right instead of up and down and added jumpToGroup
+function activateSiblingWindow({
+  windows: _windows,
+  direction,
+  startWindow,
+  jumpToGroup,
+  cycleGroup,
+}) {
+  const currentWindow = global.display.focus_window;
+
+  let windows = _windows;
+
+  if (!jumpToGroup && cycleGroup) {
+    windows = getWindowsOfSameClass(windows, currentWindow);
   }
 
-  if (windowIndex != nextWindowIndex) {
+  const windowCount = windows.length;
+  let windowIndex = windows.indexOf(currentWindow);
+  let nextWindowIndex = windowIndex;
+  const numericDirection = direction == 'left' ? -1 : 1;
+  const startWindowIndex = startWindow ? windows.indexOf(startWindow) : 0;
+
+  if (windowIndex < 0) {
+    nextWindowIndex = startWindowIndex;
+    if (windowIndex !== nextWindowIndex) {
+      Main.activateWindow(windows[nextWindowIndex]);
+    }
+    return;
+  }
+
+  nextWindowIndex = getNextWindowIndex(
+    windowIndex,
+    windowCount,
+    numericDirection
+  );
+
+  if (jumpToGroup) {
+    let currentWmClass = currentWindow.wm_class;
+    let candidateWindow = windows[nextWindowIndex];
+    let candidateWmClass = candidateWindow.wm_class;
+    let iterationsLeft = windowCount; // Prevent infinite loop
+
+    // Loop until next group is found
+    while (iterationsLeft > 0 && candidateWmClass === currentWmClass) {
+      nextWindowIndex = getNextWindowIndex(
+        nextWindowIndex,
+        windowCount,
+        numericDirection
+      );
+
+      candidateWindow = windows[nextWindowIndex];
+      candidateWmClass = candidateWindow.wm_class;
+
+      iterationsLeft--;
+    }
+
+    const nextWmClass = candidateWmClass;
+    const nextWindow = getMostRecentWindowByWMClass(windows, nextWmClass);
+
+    if (nextWindow) {
+      Main.activateWindow(nextWindow);
+      return;
+    }
+  }
+
+  if (windowIndex !== nextWindowIndex) {
     Main.activateWindow(windows[nextWindowIndex]);
   }
+}
+
+function getWindows() {
+  return global.dashToPanel.panels[0].taskbar
+    .getAppInfos()
+    .reduce((ws, appInfo) => ws.concat(appInfo.windows), []);
 }
 
 function waitForDashToPanelThenEnable(timeBetweenChecks, settings) {
@@ -49,20 +125,51 @@ function waitForDashToPanelThenEnable(timeBetweenChecks, settings) {
   // See this for more on keybindings https://www.youtube.com/watch?v=L6ewpCMkrRE
   let mode = Shell.ActionMode.ALL;
   let flag = Meta.KeyBindingFlags.NONE;
-  Main.wm.addKeybinding('activate-left', settings, flag, mode, () => {
-    console.debug(_('%s: activate-left').format(GETTEXT_DOMAIN));
-    let windows = global.dashToPanel.panels[0].taskbar
-      .getAppInfos()
-      .reduce((ws, appInfo) => ws.concat(appInfo.windows), []);
-    activateSiblingWindow(windows, 'left');
-  });
-  Main.wm.addKeybinding('activate-right', settings, flag, mode, () => {
-    console.debug(_('%s: activate-right').format(GETTEXT_DOMAIN));
-    let windows = global.dashToPanel.panels[0].taskbar
-      .getAppInfos()
-      .reduce((ws, appInfo) => ws.concat(appInfo.windows), []);
-    activateSiblingWindow(windows, 'right');
-  });
+  Main.wm.addKeybinding(
+    'dash-to-panel-activate-left',
+    settings,
+    flag,
+    mode,
+    () => {
+      console.debug(_('%s: activate-left').format(GETTEXT_DOMAIN));
+      let windows = getWindows();
+      activateSiblingWindow({ windows, direction: 'left', cycleGroup: true });
+    }
+  );
+  Main.wm.addKeybinding(
+    'dash-to-panel-activate-right',
+    settings,
+    flag,
+    mode,
+    () => {
+      console.debug(_('%s: activate-right').format(GETTEXT_DOMAIN));
+      let windows = getWindows();
+      activateSiblingWindow({ windows, direction: 'right', cycleGroup: true });
+    }
+  );
+  Main.wm.addKeybinding(
+    'dash-to-panel-activate-group-left',
+    settings,
+    flag,
+    mode,
+    () => {
+      console.debug(_('%s: activate-group-left').format(GETTEXT_DOMAIN));
+      let windows = getWindows();
+      activateSiblingWindow({ windows, direction: 'left', jumpToGroup: true });
+    }
+  );
+  Main.wm.addKeybinding(
+    'dash-to-panel-activate-group-right',
+    settings,
+    flag,
+    mode,
+    () => {
+      console.debug(_('%s: activate-group-right').format(GETTEXT_DOMAIN));
+      let windows = getWindows();
+      activateSiblingWindow({ windows, direction: 'right', jumpToGroup: true });
+    }
+  );
+
   console.debug(_('%s: keys bound').format(GETTEXT_DOMAIN));
 }
 
@@ -80,8 +187,10 @@ export default class HotkeysForActivatingSiblingsInDashToPanelExtension extends 
 
   disable() {
     console.debug(_('Disabling %s').format(GETTEXT_DOMAIN));
-    Main.wm.removeKeybinding('activate-left');
-    Main.wm.removeKeybinding('activate-right');
+    Main.wm.removeKeybinding('dash-to-panel-activate-left');
+    Main.wm.removeKeybinding('dash-to-panel-activate-right');
+    Main.wm.removeKeybinding('dash-to-panel-activate-group-left');
+    Main.wm.removeKeybinding('dash-to-panel-activate-group-right');
     this._settings = null;
   }
 }
